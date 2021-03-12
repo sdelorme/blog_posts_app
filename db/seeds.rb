f64e6937da14ffb2b9f207ef04f9b6afe894bd7c
Seth Delorme <delorme.s.m@gmail.com>
2021-03-11 21:30:31 -0600

    seeds db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+post = BlogPost.new(title: "Part One", post: "Hello there, the angel from my nightmare")
+post.save
+post = BlogPost.new(title: "Part Two", post: "The shadow in the background of the...")
+post.save
+post = BlogPost.new(title: "Part Three", post: "That was a song")
+post.save
+post = BlogPost.new(title: "Part Four", post: "That was weird")
+post.save
+
+tag = Tag.new(name: "Songsssss")
+tag.save
+tag = Tag.new(name: "Nonsense")
+tag.save
+
+
+new = BlogPostTag.new(blog_post_id: 1, tag_id: 1)
+new.save
+new = BlogPostTag.new(blog_post_id: 2, tag_id: 1)
+new.save
+new = BlogPostTag.new(blog_post_id: 3, tag_id: 2)
+new.save
+new = BlogPostTag.new(blog_post_id: 4, tag_id: 2)
+new.save
+new = BlogPostTag.new(blog_post_id: 1, tag_id: 2)
+new.save
